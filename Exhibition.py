129f88201c79f5d54675ea9a3f7221589d24e053
amiraals <159874893+amiraals@users.noreply.github.com>
2024-03-31 05:47:35 +0400

    Update Exhibition.py

--- Exhibition.py
+++ Exhibition.py
@@ -3,9 +3,9 @@ class Artwork:
 
     def __init__(self, title, artist, creation_date):
         # Initializing artwork attributes
-        self.title = title
-        self.artist = artist
-        self.creation_date = creation_date
+        self._title = title
+        self.__artist = artist
+        self.__creation_date = creation_date
 
 
 class Exhibition:
@@ -14,25 +14,25 @@ class Exhibition:
     This class has an aggregation relationship with the Artwork class"""
     def __init__(self, name, artworks=[]):
         # Initializing exhibition attributes
-        self.name = name
-        self.artworks = artworks # This parameter allows for the possibility of passing a list of Artwork objects when creating an Exhibition object
+        self.__name = name
+        self.__artworks = artworks # This parameter allows for the possibility of passing a list of Artwork objects when creating an Exhibition object
 
     def add_artwork(self, artwork):
         # Adding an artwork to the exhibition
-        self.artworks.append(artwork)
+        self.__artworks.append(artwork)
 
     def remove_artwork(self, title):
-        for artwork in self.artworks:
-            if artwork.title == title:
-                self.artworks.remove(artwork)
+        for artwork in self.__artworks:
+            if artwork._title == title:
+                self.__artworks.remove(artwork)
                 return True  # Artwork found and removed
         return False  # Artwork not found
 
     def display_info(self):
         # Displaying information about the exhibition and its artworks
-        print(f"Exhibition: {self.name}")
-        for artwork in self.artworks:
-            print(f"Artwork: {self.artworks.index(artwork) + 1}. {artwork.title}, Artist: {artwork.artist}, Date: {artwork.creation_date}")
+        print(f"Exhibition: {self.__name}")
+        for artwork in self.__artworks:
+            print(f"Artwork: {self.__artworks.index(artwork) + 1}. {artwork._title}, Artist: {artwork.__artist}, Date: {artwork.__creation_date}")
 
 
 """
